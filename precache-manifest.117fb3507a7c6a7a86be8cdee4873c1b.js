b4560f739ebba0941d5ea167431e1765b122c287
VladUs <vladustyugo@gmail.com>
2022-07-17 23:22:10 +0700

    Updates

---
+++ precache-manifest.117fb3507a7c6a7a86be8cdee4873c1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "483d24bd979618b7fdc5",
+    "url": "/recipe-book/static/css/main.e1c72a3e.chunk.css"
+  },
+  {
+    "revision": "483d24bd979618b7fdc5",
+    "url": "/recipe-book/static/js/main.483d24bd.chunk.js"
+  },
+  {
+    "revision": "80010276ce668ab252d1",
+    "url": "/recipe-book/static/js/1.80010276.chunk.js"
+  },
+  {
+    "revision": "00527e0406670882eaca",
+    "url": "/recipe-book/static/js/runtime~main.00527e04.js"
+  },
+  {
+    "revision": "56bde5100ed1db9cea4930c02477019a",
+    "url": "/recipe-book/index.html"
+  }
+];
