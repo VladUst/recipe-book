6cc96a2fd320113fa91ce959a751ec5c90b8941e
VladUs <vladustyugo@gmail.com>
2022-07-17 23:21:30 +0700

    add to gh-pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {NotFound} from './pages/NotFound';
 function App() {
   return (
     <>
-    <Router>
+    <Router basename='/recipe-book'>
     <Header/>
     <main className='container content'>
       
